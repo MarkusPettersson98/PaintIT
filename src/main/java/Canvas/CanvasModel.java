76c58e2dc36d31b9cc2587f90243db21cc90ba89
robertzetterlund <robertz@student.chalmers.se>
2018-09-14 10:52:01 +0200

    Added ToString method Co-authored-by: <markp@student.chalmers.se>

--- src/main/java/Canvas/CanvasModel.java
+++ src/main/java/Canvas/CanvasModel.java
@@ -1,4 +1,21 @@
 package Canvas;
 
 public class CanvasModel {
+
+    private int x = 5, y = 5;
+    private boolean[][] canvas = new boolean[x][y];
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < y; i++) {
+            sb.append("[");
+            for (int j = 0; j < x; j++) {
+                sb.append(canvas[j][i] + ", ");
+            }
+            sb.append("] \n");
+        }
+        return sb.toString();
+    }
+
 }
