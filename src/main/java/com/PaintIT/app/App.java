cab14b35365198aa49fa190a404c9178f5502ce4
EllenWiderstrand1 <ellen@mac>
2018-09-19 16:18:17 +0200

    Added the logo and buttons to the main menu and created a stylesheet
    Co-authored-by: HenrikLagergren <henriklagergren@hotmail.com>

--- src/main/java/com/PaintIT/app/App.java
+++ src/main/java/com/PaintIT/app/App.java
@@ -1,17 +1,34 @@
 package com.PaintIT.app;
 
-import Canvas.*;
+import MainMenu.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );         
+        launch(args);
 
+    }
+
+    @Override
+    public void start (Stage primaryStage) throws Exception{
+        primaryStage.setTitle("PainIT");
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainMenuView.fxml"));
+        Scene scene = new Scene(root, 1280, 720);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
 }
