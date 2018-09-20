6fdeb6530c9952877abd194e90a3da0e6b7dbcb3
henriklagergren <henriklagergren@hotmail.com>
2018-09-20 14:05:30 +0200

    Added Player-class and Player-tests
    Co-authored-by: EllenWiderstrand <EllenWi@student.chalmers.se>

---
+++ src/test/java/Game/PlayerTest.java
@@ -0,0 +1,29 @@
+package Game;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class PlayerTest {
+    public Player player;
+
+    @Before
+    public void setUp(){
+        player = new Player("Greta");
+    }
+
+    @Test
+    public void nameTest(){
+        assertTrue(player.getName().equals("Greta"));
+    }
+
+    @Test
+    public void stateTest(){
+        player.setDrawer(true);
+        assertTrue(player.isDrawer());
+    }
+
+}
