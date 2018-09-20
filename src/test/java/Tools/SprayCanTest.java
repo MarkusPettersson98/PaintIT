0ecfc34287fd54acf883170902cc33c161f8c794
robertzetterlund <robertz@student.chalmers.se>
2018-09-20 22:14:24 +0200

    wrote tests for spraycan and eraser

---
+++ src/test/java/Tools/SprayCanTest.java
@@ -0,0 +1,38 @@
+package Tools;
+
+import Canvas.CanvasController;
+import javafx.scene.paint.Color;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SprayCanTest {
+
+    private SprayCan sprayCan;
+    public CanvasController canvasController;
+    @Before
+    public void beforeSprayCanTest(){
+        sprayCan = new SprayCan();
+        canvasController = new CanvasController(5,5);
+        sprayCan.addObserver(canvasController);
+    }
+
+
+    @Test
+    public void testSpray() {
+        sprayCan.setRadius(5);
+        canvasController.fillCanvas(Color.WHITE);
+        sprayCan.setOdds(0.20);
+        String dummyCanvas =
+                        "[ [ 1.0, 1.0, 1.0 ] [ 1.0, 1.0, 1.0 ] [ 1.0, 1.0, 1.0 ] [ 1.0, 1.0, 1.0 ] [ 1.0, 1.0, 1.0 ] ] \n" +
+                        "[ [ 1.0, 1.0, 1.0 ] [ 1.0, 1.0, 1.0 ] [ 1.0, 1.0, 1.0 ] [ 1.0, 1.0, 1.0 ] [ 1.0, 1.0, 1.0 ] ] \n" +
+                        "[ [ 1.0, 1.0, 1.0 ] [ 1.0, 1.0, 1.0 ] [ 1.0, 1.0, 1.0 ] [ 1.0, 1.0, 1.0 ] [ 1.0, 1.0, 1.0 ] ] \n" +
+                        "[ [ 1.0, 1.0, 1.0 ] [ 1.0, 1.0, 1.0 ] [ 1.0, 1.0, 1.0 ] [ 1.0, 1.0, 1.0 ] [ 1.0, 1.0, 1.0 ] ] \n" +
+                        "[ [ 1.0, 1.0, 1.0 ] [ 1.0, 1.0, 1.0 ] [ 1.0, 1.0, 1.0 ] [ 1.0, 1.0, 1.0 ] [ 1.0, 1.0, 1.0 ] ] \n";
+        sprayCan.apply(2,2);
+        // checks if the canvas has changed at all, if not identical as dummyCanvas, it has changed.
+        // this test is flawed, because it is possible to fail "the odds", meaning not painting. 0.20^25, which is 0,3,3554432e-18 %.
+        assertFalse(dummyCanvas.equals(canvasController.toString()));
+    }
+}
