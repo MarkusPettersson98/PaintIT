ba15b3d9595dc53b38024bd4f7c6fd8be4907296
AronSeamountain <aronsj@student.chalmers.se>
2018-09-22 10:20:39 +0200

    added working constructor to tileSlot which adds button text

--- src/main/java/ViewObjects/TileSlot.java
+++ src/main/java/ViewObjects/TileSlot.java
@@ -1,11 +1,16 @@
 package ViewObjects;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
+
 public class TileSlot extends AnchorPane {
+
+    @FXML Button tileButton;
+
     private String filePath = "/fxml/tileSlot.fxml";
     public TileSlot(){
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(filePath));
@@ -16,8 +21,7 @@ public class TileSlot extends AnchorPane {
         } catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
-    
+        tileButton.setText("A");
     }
 
 }
