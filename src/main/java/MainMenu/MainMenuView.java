d4d3574bd5adecb6bc87e66f7675947a1997d90c
EllenWiderstrand <ellenwi@student.chalmers.it>
2018-09-27 13:47:09 +0200

    Added a lightbox for how to play
    Co-authored-by: HenrikLagergren <henriklagergren@hotmail.com>

--- src/main/java/MainMenu/MainMenuView.java
+++ src/main/java/MainMenu/MainMenuView.java
@@ -1,20 +1,29 @@
 package MainMenu;
 
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.event.EventHandler;
 import javafx.scene.layout.AnchorPane;
+import sun.plugin.javascript.navig.Anchor;
 
 import java.io.IOException;
 
 public class MainMenuView extends AnchorPane{
 
+    @FXML private AnchorPane mainMenuAnchorPane;
     @FXML private Button play;
     @FXML private Button howToPlay;
     @FXML private Button highScore;
+
+    @FXML private AnchorPane howToPlayAnchorPane;
+    @FXML private AnchorPane lightBoxAnchorPane;
+    @FXML private ImageView closeButtonImageView;
 
     public MainMenuView () {
 
@@ -29,6 +38,8 @@ public class MainMenuView extends AnchorPane{
             e.printStackTrace();
         }
 
+        showMainMenu();
+
         play.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -40,6 +51,7 @@ public class MainMenuView extends AnchorPane{
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("tjena");
+                showHowToPlay();
             }
         });
 
@@ -50,6 +62,65 @@ public class MainMenuView extends AnchorPane{
             }
         });
 
+        closeButtonImageView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                showMainMenu();
+                closeButtonMouseClickedOrExited();
+            }
+        });
+
+        closeButtonImageView.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                closeButtonMouseEntered();
+            }
+        });
+
+        closeButtonImageView.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                closeButtonMouseClickedOrExited();
+            }
+        });
+
+        lightBoxAnchorPane.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                showMainMenu();
+            }
+        });
+
+        howToPlayAnchorPane.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                mouseTrap(event);
+            }
+        });
+
+    }
+
+    private void closeButtonMouseEntered(){
+        String path = "images/icon_close_hover.png";
+        closeButtonImageView.setImage((new Image(getClass().getClassLoader().getResourceAsStream((path)))));
+    }
+
+    private void closeButtonMouseClickedOrExited(){
+        String path = "images/icon_close.png";
+        closeButtonImageView.setImage((new Image(getClass().getClassLoader().getResourceAsStream((path)))));
+    }
+
+    private void showMainMenu (){
+        mainMenuAnchorPane.toFront();
+    }
+
+    private void showHowToPlay (){
+        lightBoxAnchorPane.toFront();
+        howToPlayAnchorPane.toFront();
+    }
+
+    private void mouseTrap(Event event){
+        event.consume();
     }
 
 }
