72c25d17feedadaec41dd24e5f2db9b45671e7df
henriklagergren <henriklagergren@hotmail.com>
2018-10-02 10:12:56 +0200

    Changed name of dictionary
    Co-authored-by: EllenWiderstrand <EllenWi@student.chalmers.se>

--- src/main/java/WordAndGuess/Dictionary.java
+++ src/main/java/WordAndGuess/Dictionary.java
@@ -1,7 +1,12 @@
 package WordAndGuess;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+import com.google.gson.*;
 
 public class Dictionary {
 
@@ -9,6 +14,17 @@ public class Dictionary {
 
     Dictionary(){
 
+    }
+
+
+    private void generateWordList() throws IOException{
+        Gson gson = new GsonBuilder().create();
+
+        Reader reader = new InputStreamReader(getClass().getResourceAsStream("/dictionary.json"), "UTF-8");
+
+        while(true){
+            dictionary.add(new Word());
+        }
     }
 
 
