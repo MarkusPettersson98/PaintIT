fb71e7ae3b0df4e95d4cc90f7e5368fd2513c872
EllenWiderstrand <ellenwi@student.chalmers.se>
2018-10-10 22:56:21 +0200

    Added Javadoc for Team
    Co-authored-by: HenrikLagergren <henriklagergren@hotmail.com>

--- src/main/java/Game/Team.java
+++ src/main/java/Game/Team.java
@@ -11,12 +11,35 @@ import java.util.Random;
 
 public class Team {
 
+    /**
+     * The first {@link Player}.
+     */
     private final Player playerOne;
+
+    /**
+     * The second {@link Player}.
+     */
     private final Player playerTwo;
+
+    /**
+     * The name of the team, ie a combination of the players' names.
+     */
     @Getter private final String teamName;
+
+    /**
+     * The team's streak is the number of times the team has guessed a word correctly.
+     */
     @Getter private int streak;
 
+
+    /**
+     * The {@link Player} that is drawing the painting.
+     */
     private Player drawer;
+
+    /**
+     * The {@link Player} that is guessing what the drawer painted.
+     */
     private Player guesser;
 
     private Random rand = new Random();
@@ -28,12 +51,21 @@ public class Team {
         setGuesserAndDrawer();
     }
 
+    /**
+     * Increments the {@link Team#streak} with one.
+     */
     public void incrementStreak() { streak++; }
 
+    /**
+     * Sets the {@link Team#streak} to zero.
+     */
     public void resetStreak() {
         streak = 0;
     }
 
+    /**
+     * Randomly sets which {@link Player} who is the {@link Team#guesser} and who is the {@link Team#drawer}.
+     */
     private void setGuesserAndDrawer (){
         int randomNumber = rand.nextInt(2);
         if (randomNumber == 0){
@@ -46,6 +78,10 @@ public class Team {
         }
     }
 
+    /**
+     * Changes who is the guesser/drawer. The {@link Player} who was the {@link Team#drawer}
+     * changes to the {@link Team#guesser} and vice versa.
+     */
     public void changeDrawer (){
         Player temporary = drawer;
         drawer = guesser;
@@ -60,10 +96,18 @@ public class Team {
         return new Pair<>(playerOne.getName(), playerTwo.getName());
     }
 
+    /** Returns the guesser's name.
+     *
+     * @return returns the name of {@link Team#guesser}
+     */
     public String getGuesserName (){
         return guesser.getName();
     }
 
+    /**Returns the drawer's name.
+     *
+     * @return returns the name of {@link Team#drawer}
+     */
     public String getDrawerName (){
         return drawer.getName();
     }
