6e17afdad9244fd36cf452a579945d9d98cd7907
EllenWiderstrand <ellenwi@student.chalmers.se>
2018-10-11 18:56:52 +0200

    Added test for WordRevealView.
    Co-authored-by: HenrikLagergren <henriklagergren@hotmail.com>

---
+++ src/test/java/Views/WordRevealViewTest.java
@@ -0,0 +1,34 @@
+package Views;
+
+import Game.GameSession;
+import Game.Team;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+public class WordRevealViewTest extends ApplicationTest {
+
+    GameSession gameSession;
+    Scene scene;
+
+    @Override
+    public void start (Stage stage){
+        gameSession = new GameSession();
+        scene = new Scene(gameSession.getCurrentPane());
+
+        Team team = new Team("Test One", "Test Two");
+        gameSession.addTeam(team);
+        gameSession.show(WordRevealView.class.getSimpleName());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Test
+    public void revealNowTest (){
+        clickOn(".button-play");
+        assert (gameSession.getCurrentPane().getChildren().toString().contains("PaintingView"));
+    }
+
+
+}
