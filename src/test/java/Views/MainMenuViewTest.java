aad5d95b4ba6a5646e1496ecb18124e89a90aaaa
henriklagergren <henriklagergren@hotmail.com>
2018-10-11 18:31:57 +0200

    Added Test for MainMenuView
    Co-authored-by: EllenWiderstrand <EllenWi@student.chalmers.se>

--- src/test/java/Views/MainMenuViewTest.java
+++ src/test/java/Views/MainMenuViewTest.java
@@ -1,11 +1,30 @@
 package Views;
 
-import org.junit.Before;
+import Game.GameSession;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
-import static org.junit.Assert.*;
-
 public class MainMenuViewTest extends ApplicationTest{
-    
+
+    GameSession gameSession;
+    Scene scene;
+
+    @Override
+    public void start(Stage stage) {
+        gameSession = new GameSession();
+
+        scene = new Scene(gameSession.getCurrentPane());
+
+        gameSession.show(MainMenuView.class.getSimpleName());
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Test
+    public void playButtonTest(){
+      
+    }
 }
