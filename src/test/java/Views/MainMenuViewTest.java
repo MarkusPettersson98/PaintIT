32dd687e2835dad310760ccd62e6e5038c7b126d
henriklagergren <henriklagergren@hotmail.com>
2018-10-11 16:50:06 +0200

    Added another TextFX depencies and created a testfolder for views
    Co-authored-by: EllenWiderstrand <EllenWi@student.chalmers.se>

---
+++ src/test/java/Views/MainMenuViewTest.java
@@ -0,0 +1,11 @@
+package Views;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import static org.junit.Assert.*;
+
+public class MainMenuViewTest extends ApplicationTest{
+    
+}
