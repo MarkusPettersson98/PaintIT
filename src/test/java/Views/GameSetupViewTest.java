aa63f31623cc2f8127e7462b03143bd837523b37
EllenWiderstrand <ellenwi@student.chalmers.se>
2018-10-12 11:49:02 +0200

    Added tests for the new methods in GameSetupView

--- src/test/java/Views/GameSetupViewTest.java
+++ src/test/java/Views/GameSetupViewTest.java
@@ -1,7 +1,9 @@
 package Views;
 
 import Game.GameSession;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
@@ -10,6 +12,13 @@ public class GameSetupViewTest extends ApplicationTest{
 
     GameSession gameSession;
     Scene scene;
+    Label playerOneWrongLabel;
+    Label playerTwoWrongLabel;
+
+    public <T extends Node> T find(final String query) {
+        /** TestFX provides many operations to retrieve elements from the loaded GUI. */
+        return lookup(query).query();
+    }
 
     @Override
     public void start (Stage stage){
@@ -20,7 +29,11 @@ public class GameSetupViewTest extends ApplicationTest{
         gameSession.show(GameSetupView.class.getSimpleName());
         stage.setScene(scene);
         stage.show();
+
+        playerOneWrongLabel = (Label) find("#playerOneWrongName");
+        playerTwoWrongLabel = (Label) find("#playerTwoWrongName");
     }
+
 
     @Test
     public void savingNamesTest () {
@@ -34,8 +47,28 @@ public class GameSetupViewTest extends ApplicationTest{
 
     @Test
     public void startDrawingTest (){
+        clickOn("#player1TextField");
+        write("Test One");
+        clickOn("#player2TextField");
+        write("Test Two");
         clickOn(".button-play");
         assert (gameSession.getCurrentPane().getChildren().toString().contains("WordRevealView"));
+    }
+
+    @Test
+    public void noNamesEnteredTest (){
+        clickOn(".button-play");
+        assert (playerOneWrongLabel.getText().equals("Enter a name!") &&
+                playerTwoWrongLabel.getText().equals("Enter a name!"));
+    }
+
+    @Test
+    public void oneNameEnteredTest (){
+        clickOn("#player1TextField");
+        write("Test One");
+        clickOn(".button-play");
+        assert (playerOneWrongLabel.getText().equals("") &&
+                playerTwoWrongLabel.getText().equals("Enter a name!"));
     }
 
     @Test
